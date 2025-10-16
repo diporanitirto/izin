17ee26f9c16706b68cfa7f2bef249eb765f03c6f
ZulGanzy <zultest85@gmail.com>
2025-10-16 22:40:12 +0700

    Add icon metadata for application branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Surat Ijin Tidak Mengikuti Pramuka",
   description: "Surat izin tidka mengikuti pramuka.",
+  icons: {
+    icon: "/assets/logo-diporani.png",
+  },
 };
 
 export default function RootLayout({
